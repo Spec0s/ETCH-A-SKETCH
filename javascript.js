793e7af4f57fd571d2af5e9da984dbc4e1b92827
Spec0s <edgaras2112@mail.ru>
2022-04-14 20:17:42 +0300

    added grid square and change color on mouse over

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-alert("Hello")
+//                  Create squares                       //
+const mainGrid = document.querySelector(".main-grid");
+
+
+var squareNaming = []
+
+for (var i = 1; i <= 256; i++) {
+    squareNaming.push("square" + i)
+}
+
+squareNaming.forEach(function (el) {
+    el = document.createElement("div");
+    mainGrid.appendChild(el);
+});
+
+
+
+//                      Change color on mouse over              //
+var className=document.querySelector(".square-class");
+mainGrid.addEventListener("mouseover",function(e){
+    e.target.classList.add("change-color");
+});
+
+
+
+
+
+
+
