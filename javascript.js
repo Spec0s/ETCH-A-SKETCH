b8a2463d81651cad8cf0cffa4973d9db47425f4c
Spec0s <edgaras2112@mail.ru>
2022-04-15 19:36:53 +0300

    added clear grid button

--- javascript.js
+++ javascript.js
@@ -1,25 +1,44 @@
+var x = 256;
+
+
+//                  Reset button + add grid number          //
+
+
+var gridReset = document.querySelector(".grid-reset")
+gridReset.addEventListener('click', function () {
+
+    // let grid = prompt("Enter grid number");
+    // x = Math.pow(grid, 2);
+    className.forEach(className => className.removeAttribute("style"));
+})
+
 //                  Create squares                       //
-const mainGrid = document.querySelector(".main-grid");
 
 
 var squareNaming = []
+const mainGrid = document.querySelector(".main-grid");
 
-for (var i = 1; i <= 256; i++) {
+for (var i = 1; i <= x; i++) {
     squareNaming.push("square" + i)
 }
 
 squareNaming.forEach(function (el) {
     el = document.createElement("div");
+    el.className = "square-class";
     mainGrid.appendChild(el);
 });
 
 
 
 //                      Change color on mouse over              //
-var className=document.querySelector(".square-class");
-mainGrid.addEventListener("mouseover",function(e){
-    e.target.classList.add("change-color");
-});
+var className = document.querySelectorAll(".square-class");
+className.forEach(className => className.addEventListener("mouseover", function (e) {
+    e.target.style.cssText = "background-color: green;"
+}));
+
+
+
+
 
 
 
