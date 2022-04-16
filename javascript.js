5b12aaa0d0a487b388f218fbdda90bbc59a1e7ea
Spec0s <edgaras2112@mail.ru>
2022-04-16 15:02:44 +0300

    added prompt to enter grid number on button click

--- javascript.js
+++ javascript.js
@@ -1,40 +1,65 @@
+
+//                      Variables                           //
 var x = 256;
 
 
 //                  Reset button + add grid number          //
 
-
 var gridReset = document.querySelector(".grid-reset")
 gridReset.addEventListener('click', function () {
+    removeAllChildNodes(mainGrid);
 
-    // let grid = prompt("Enter grid number");
-    // x = Math.pow(grid, 2);
-    className.forEach(className => className.removeAttribute("style"));
+    let grid = prompt("Enter grid number");
+    x = Math.pow(grid, 2);
+    let boxSize = (500 / grid) + "px";
+
+    var squareNaming = []
+
+    for (var i = 1; i <= x; i++) {
+        squareNaming.push("square" + i)
+    }
+
+    squareNaming.forEach(function (el) {
+        el = document.createElement("div");
+        el.style.flexBasis = boxSize;
+        mainGrid.appendChild(el);
+    });
+    var class1 = document.querySelectorAll(".main-grid>div");
+    class1.forEach(className => className.addEventListener("mouseover", function (e) {
+        e.target.style.cssText += "background-color: green;"
+    }));
 })
 
+
 //                  Create squares                       //
-
-
 var squareNaming = []
-const mainGrid = document.querySelector(".main-grid");
-
+var mainGrid = document.querySelector(".main-grid");
 for (var i = 1; i <= x; i++) {
     squareNaming.push("square" + i)
 }
 
 squareNaming.forEach(function (el) {
     el = document.createElement("div");
-    el.className = "square-class";
+    el.style.flexBasis = "31.25px";
     mainGrid.appendChild(el);
 });
 
 
-
 //                      Change color on mouse over              //
-var className = document.querySelectorAll(".square-class");
+var className = document.querySelectorAll(".main-grid>div");
 className.forEach(className => className.addEventListener("mouseover", function (e) {
-    e.target.style.cssText = "background-color: green;"
+    e.target.style.cssText += "background-color: green;"
 }));
+
+
+//                      Removes all squares function                //
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
 
 
 
